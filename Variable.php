b245a0d866fddbd04d2bf87581783f5119bb7db6
ahdianmirza <ahdianmirza@gmail.com>
2022-12-28 13:51:10 +0700

    menambahkan file latihan variable

---
+++ Variable.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = "Ahdian";
+$age = 21;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
+
+// variable variables
+$name = "ahdian";
+$$name = "keren";
+
+echo "\$name = ";
+echo $name;
+echo "\n";
+
+echo "\$ahdian = ";
+echo $ahdian;
+echo "\n";
